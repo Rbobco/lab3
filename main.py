138beb590303e7e9d0db18a72c47115caded00bc
Rbobco <Bobokalonov.Roman@yandex.ru>
2025-12-19 11:52:38 +0300

    Output fix, PEP8 polishing

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from collatz import syracuse_sequence, syracuse_max
 
+
 def main():
     while True:
         n = int(input("Введите положительное целое число N (0 для выхода): "))
-        if  n == 0:
+        if n == 0:
             break
         elif n < 0:
-            print ("N должно быть положительным")
+            print("N должно быть положительным")
             continue
         seq = syracuse_sequence(n)
-        max_val = syracuse_sequence(n)
-        print("Поселдовательность для" + n + ":" + seq)
-        print("Максимум:" + max_val)
+        max_val = syracuse_max(n)
+        print("Поселдовательность для" + str(n) + ":" + str(seq))
+        print("Максимум:" + str(max_val))
+
 
 main()
