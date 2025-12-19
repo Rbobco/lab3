808192e4158648d5e2420b7f248e6ea6298ad8af
Rbobco <Bobokalonov.Roman@yandex.ru>
2025-12-19 11:31:25 +0300

    syracuse_max function

--- collatz.py
+++ collatz.py
@@ -11,3 +11,16 @@ def syracuse_sequence(n: int) -> List[int]:
             n = 3 * n + 1
         sequence.append(n)
     return sequence
+
+def syracuse_max(n: int) -> List[int]:
+    if n <= 0:
+        raise ValueError("N должно быть положительным целым числом")
+    max_val = n
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        if n > max_val:
+            max_val = n
+    return max_val
